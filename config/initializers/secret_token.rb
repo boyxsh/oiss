76aac4a29e3494f7a13b0071fe5e19297307de95
boyxsh <boyxsh@gmail.com>
2010-11-23 21:37:39 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oiss::Application.config.secret_token = '97db3ae37a9ee4bf2686a230f6315a16a9f161bf268909e0c47eb6049f468d686808e77d1bb81eaccdce244593ebf5b7388ef708bd5d9d2250392a67b99a3591'
