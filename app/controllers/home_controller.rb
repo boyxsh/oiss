1a643d6e32a19fe27f45823997ee3cebbcbc1dfa
boyxsh <boyxsh@gmail.com>
2011-01-29 12:14:00 +0800

    add admin links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,5 +13,6 @@ class HomeController < ApplicationController
 
   def news
     @news = News.all
+    @show_news = params[:id].blank? ? @news.first :  News.find_by_id(params[:id])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
   def admin?
-      p session[:admin]
       session[:admin]
   end
+
+  def show_goto_admin
+      link_to 'Goto admin', admin_index_path
+  end
+
   def show_logout
     link_to 'Logout', admin_logout_path 
   end
