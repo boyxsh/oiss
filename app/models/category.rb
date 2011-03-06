e38d0afb0631da16360f2a28545f4669145dcff8
boyxsh <boyxsh@gmail.com>
2011-03-06 16:14:57 +0800

    add s3 support

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ActiveRecord::Base
   has_many :pictures
-  has_attached_file :category, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"
+  has_attached_file :category,
+    :storage => :s3,
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :path => "/:style/:id/:filename"
 
   has_one:description
   accepts_nested_attributes_for :description
