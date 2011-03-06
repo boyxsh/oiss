94a74ccd89420278456e7c7dd9f535f248066afa
bob <yinliangdl@163.com>
2011-03-06 10:01:48 +0800

    For s3 uploading.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :pictures
-  has_attached_file :category
+  has_attached_file :category, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => "/:style/:filename"
 
   has_one:description
   accepts_nested_attributes_for :description
